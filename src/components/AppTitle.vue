b16a4bcf33bbe74ba3d53f1a2aa2a1c17e06ad9e
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-30 17:58:51 +0200

    feat: add componente AppTitl

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data() {
+      return{
+        helleVite: 'La mia prima app con Vite!',
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ helleVite }}</h1>
+</template>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
